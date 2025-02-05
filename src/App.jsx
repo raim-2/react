37d3d83961aea822f06a86ecb9c7815998814653
min <earnestga@naver.com>
2025-02-05 17:55:20 +0900

    이벤트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,43 @@
  * 컴포넌트가 증가할 수록 하나의 파일에 여러개의 컴포넌트를 넣는것은 좋지 x
  */
 // import componentImg from "./assets/components.png";
+import { useState } from 'react'; // 리액트에서 use로 시작하는 함수는 리액트 Hooks라 함
+
 import CorecConcept from "./components/CoreConcept.jsx";
 import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 
 /* <Header></Header> 또는 <Header /> */
+/**
+ * 컴포넌트 함수를 코드는 한 번 밖에 실행되지 않는다.
+ * → 일반적인 방법으로는 데이터가 변경되도 ui가 업데이트 되지 않음.
+ * state라는 개념을 이용해서 처리할 수 있다.
+ *
+ * 이 때 사용하는 것이 use로 시작하는 함수인 리액트 hooks
+ * useState를 사용한다. → useState는 리액트 컴포넌트나 리액트 hooks 안에서 호출해야 함
+ * 또한 다른 함수(함수나 if문 또는 loop도 x)와 중첩되서 호출되선 안되고,
+ * → 컴포넌트 함수의 최상위에서 호출되야 함함
+ */
+
 function App() {
+    // const stateArray =useState('Please click a button');
+    // let tabContent = 'Please click a button';
+    
+    // 이름은 자유롭게 지을 수 있나 아래 이름이 일반적, 첫번째 요소는 초기값
+    // 두번째 요소는 항상 함수이며, 상태를 업데이트 하기 위해 실행되고, 저장 값을 업데이트한다.
+    
+    const [ selectedTopic, setSelecTopic] =useState('Please click a button');
+    
+    function handleClick(selectedButton) {
+        // selectedButton => 'components', 'jsx', 'props', 'state'
+        console.log(selectedButton + ' selected!');
+        setSelecTopic(selectedButton);
+        console.log(setSelecTopic);
+    }
+    
+    console.log('app component executing');
+
     return (
         <div>
             {/* <Header></Header> */}
@@ -27,7 +58,7 @@ function App() {
                         {/* 재사용하면서 할 때마다 다른 데이터 전달하고자 할 때 사용 */}
                         {/* 커스텀 컴포넌트에는 props라는 커스텀 속성 (컴포넌트를 설정하는 개념이라고 해서 props라 불림)도 지정 가능하다.
                         리액트가 props를 키로, 해당 속성의 값을 값으로 해 하나의 객체로 그룹화한다. */}
-                      
+                    
                         {/* <CorecConcept id={CORE_CONCEPTS[0]} />
                         <CorecConcept id={CORE_CONCEPTS[1]} />
                         <CorecConcept id={CORE_CONCEPTS[2]} />
@@ -35,16 +66,30 @@ function App() {
 
                         {/* 스프레드연산자 사용해 객체의 모든 키, 값을 뽑을 수 있다. */}
                         <CorecConcept
-                          title={CORE_CONCEPTS[0].title}
-                          description={CORE_CONCEPTS[0].description}
-                          image={CORE_CONCEPTS[0].image}
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
                         />
                         <CorecConcept {...CORE_CONCEPTS[1]} />
                         <CorecConcept {...CORE_CONCEPTS[2]} />
                         <CorecConcept {...CORE_CONCEPTS[3]} />
                     </ul>
                 </section>
-                <h2>Time to get started!</h2>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        {/* 컴포넌트 합성 */}
+                        {/* <TabButton label="Components"></TabButton> */}
+                        {/* <TabButton onSelect={handleClick}>Components</TabButton> */}
+                        {/* 이벤트를 핸들링하는 함수의 실행을 다른 함수로 감싸면,
+                        그 다른 함수가 이벤트 핸들링의 prop(속성)의 값으로 전달된다. */}
+                        <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleClick('state')}>State</TabButton>
+                    </menu>
+                    {selectedTopic}
+                </section>
             </main>
         </div>
     );
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,39 @@
+// export default function TabButton(props) {
+/**
+ * children prop는 별도 설정한 속성이 아니라,
+ * 리액트에서 설정해둔 prop이다.
+ * 컴포넌트 안의 텍스트의 내용이 포함되어 있다.
+ */
+
+// 1. children 사용
+export default function TabButton({children, onSelect}) {
+    // 이벤트연결하기
+    // 1. 일반 명령식 자바스크립트
+    // document.querySelector('button').addEventListener('click', () => {})
+
+    // 2. 그 대신 리액트에서 제공되는 prop 사용하자
+    // on이벤트를 사용해라 → 괄호를 넣으면, 바로 실행되기 때문에 괄호를 넣어주지 x
+    console.log('tabbbuton component executing')
+    return (
+        <li>
+            {/* <button>{props.children}</button> */}
+
+            <button onClick={onSelect}>{children}</button>
+        </li>
+    )
+}
+
+//2 2. 속성 사용
+// export default function TabButton({label}) {
+//     return (
+//         <li>
+//             {/* <button>{props.label}</button> */}
+//             <button>{label}</button>
+//         </li>
+//     )
+// }
+
+/**
+ * 둘 다 같은 결과를 출력함 → 결국은 선택이다.
+ * children을 사용할 것인지, 별도 속성을 사용할 것인지
+ */
